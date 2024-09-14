8bc911938be43b0178d7cc68da9275992626a196
laibathecoder <167254061+laibathecoder@users.noreply.github.com>
2024-09-14 11:26:33 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,81 @@
+#! /usr/bin/env node
+
+// OOP MY BANK
+
+interface bankAccount1 {
+    accountBalance: number;
+    credit(amount: number): void;
+    debit(amount: number): void;
+  }
+  
+  class BankAccount implements bankAccount1 {
+    accountBalance: number;
+  
+    constructor(accountBalance: number) {
+      this.accountBalance = accountBalance;
+    }
+  
+    public credit(amount: number) {
+      if (amount > 0) {
+        this.accountBalance += amount;
+        console.log("Your credit is successfully transferred to the new account.");
+      } else {
+        console.log("Your credit was unsuccessful in transferring to the new account.");
+      }
+    }
+  
+    public debit(amount: number) {
+      if (amount > 0 && amount < this.accountBalance) {
+        this.accountBalance -= amount;
+        console.log("Your debit is successfully transferred to the new accountand your remaning balance is: " + this.accountBalance);
+      } else {
+        console.log("Your debit was unsuccessful in transferring to the new account.");
+      }
+    }
+  }
+  
+  class Customer {
+    bankcustomer: {
+      firstname: string;
+      lastname: string;
+    };
+    age: number;
+    gender: string;
+    mobileNumber: number;
+    bankaccount: bankAccount1;
+  
+    constructor(
+      bankcustomer: { firstname: string; lastname: string },
+      age: number,
+      gender: string,
+      mobileNumber: number,
+      bankaccount: bankAccount1
+    ) {
+      this.bankcustomer = bankcustomer;
+      this.age = age;
+      this.gender = gender;
+      this.mobileNumber = mobileNumber;
+      this.bankaccount = bankaccount;
+    }
+  
+    public visible() {
+      console.log("Name: " + this.bankcustomer.firstname + " " + this.bankcustomer.lastname);
+      console.log("Age: " + this.age);
+      console.log("Gender: " + this.gender);
+      console.log("Mobile Number: " + this.mobileNumber);
+      console.log("Amount in Bank: " + this.bankaccount.accountBalance);
+      console.log();
+    }
+  }
+  
+  const A1 = new BankAccount(100000);
+  const C1 = new Customer({ firstname: "Zoya", lastname: "Naz" }, 20, "Female", 348556879, A1);
+  C1.visible();
+  C1.bankaccount.debit(20000);
+  console.log();
+  
+  const A2 = new BankAccount(30000);
+  const C2 = new Customer({ firstname: "Huzaifa", lastname: "Ali" }, 25, "Male", 16482334, A2);
+  C2.visible();
+  C2.bankaccount.credit(20000);
+  console.log();  
